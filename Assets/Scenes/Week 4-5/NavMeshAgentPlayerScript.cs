21c40286f3e325200b90ac492ee6d622003a20ef
Krishna Ramdam <krishna.ramdam@student.griffith.ie>
2023-05-12 23:11:33 +0100

    All labs

---
+++ Assets/Scenes/Week 4-5/NavMeshAgentPlayerScript.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NavMeshAgentPlayerScript : MonoBehaviour
+{
+    //public Transform goal;
+
+    private UnityEngine.AI.NavMeshAgent agent;
+
+    public float projectileLifetime;
+    public float projectileSpeed;
+    public Transform shootPoint;
+    public GameObject projectile;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                agent.destination = hit.point;
+            }
+        }
+        
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                
+                //instantiate projectile at shootpoint
+                GameObject projectile = Instantiate(this.projectile, shootPoint.transform.position, Quaternion.identity);
+                projectile.GetComponent<ProjectileScript>().projectileType = 1;
+                projectile.GetComponent<ProjectileScript>().projectileLife = projectileLifetime;
+                projectile.GetComponent<Rigidbody>().AddForce((new Vector3(hit.point.x - shootPoint.transform.position.x, shootPoint.transform.position.y, hit.point.z - shootPoint.transform.position.z)).normalized * projectileSpeed, ForceMode.Impulse);
+            }
+        }
+
+    }
+}
---
+++ Assets/Scenes/week 6/SphereScript.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class SphereScript : MonoBehaviour
+{
+      
+    public Slider slider;
+    float sliderValue;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        sliderValue = slider.value;
+        transform.localScale = new Vector3(sliderValue, sliderValue, sliderValue);
+    }
+
+    public void ChangeColour(){
+        GetComponent<Renderer>().material.color = Color.green;
+    }
+
+    public void EnableRigidBody(){
+        GetComponent<Rigidbody>().useGravity = true;
+    }
+}
